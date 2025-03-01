16a443ec1cef1f969df8945f6a4d64a951bc30cb
Sabbir-96 <sabbirislam9404@gmail.com>
2025-03-02 01:37:24 +0600

    update a project all complete js part

---
+++ js/qus.js
@@ -0,0 +1,5 @@
+// back do desk to index html
+document.getElementById('back-desk')
+    .addEventListener("click", function(){
+        window.location.href="../index.html";
+    })
--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 // background color change 
-const bgColors = ["#cfecf7", "#494afd", "	#008080", "#FFC0CB", "#e74c3c", "#2ecc71"];
-document.getElementById("bg-colo-btn")
-    .addEventListener('click', function(){
-        const BgRandomColor = bgColors[Math.floor(Math.random() * bgColors.length)];
-        document.body.style.backgroundColor = BgRandomColor;
-    })
+
 
 // dynamic date in js
 
 
- function updateDateAndTime() {
+ function updateDate() {
     const todayDate = new Date();
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     const month = months[todayDate.getMonth()];
@@ -21,5 +16,64 @@ document.getElementById("bg-colo-btn")
 
 }
 
-updateDateAndTime();
+updateDate();
+
+
+// click the button and some featurs working
+
+const buttonComplete = document.querySelectorAll('.btn-complete');
+let clickCount = 0;
+
+for(let i = 0; i < buttonComplete.length; i++ ){
+    const btnComplete = buttonComplete[i];
+    const btnCount = 0;
+    btnComplete.addEventListener('click', function(event){
+        alert("Board updated successfully");
+
+        let checkBoxCount = parseInt(document.getElementById('check-box-count').innerText);
+        let taskCount = parseInt(document.getElementById('task-count').innerText);
+       
+        checkBoxCount++;
+        taskCount--;
+        
+
+        document.getElementById("check-box-count").innerText = checkBoxCount;
+        document.getElementById("task-count").innerText = taskCount;
+        const cartTitle = document.querySelectorAll('.cart-title');
+        const nowTime = new Date();
+        const currentTime = nowTime.toLocaleTimeString();
+  
+        const history = document.getElementById('history-list');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <p class="p-3 text-sm mt-3 bg-blue-50 rounded-lg">You have Complete The Task ${cartTitle[i].innerText} at ${currentTime}</p>
+        `;
+        history.appendChild(div);
+
+        event.target.disabled = true;
+        
+        clickCount++;
+        if (clickCount === buttonComplete.length) {
+            alert("Congratulations, you have completed all the current tasks");
+        }
+    });
+}
+
+// clear history click js part
+document.getElementById('clear-history-btn')
+    .addEventListener("click", function(){
+        const history = document.getElementById('history-list');
+        history.innerHTML = "";
+    })
+
+
+// discover button click shift a anothe page
+document.getElementById("discover-btn")
+    .addEventListener('click', function(){
+        window.location.href='../qus.html';
+    })
+
+
+
+
 
